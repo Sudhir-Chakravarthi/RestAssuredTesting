bf498961b3679748ee34047fe2b34c2e7df3d166
sudhir-chakravarthi <sudhirchakravarthin@gmail.com>
2020-08-20 15:39:10 +0530

    Second Commit Added Logger Response for Failed Response

--- src/test/java/com/student/loggingExamples/LogginResponseValues.java
+++ src/test/java/com/student/loggingExamples/LogginResponseValues.java
@@ -10,17 +10,60 @@ import com.restTests.TestBase;
 public class LogginResponseValues extends TestBase{
 
 	@DisplayName("Logging the Response Information")
-	@Test
+	//@Test
 		public void test002() {
 			System.out.println("-------------Logging Response Information-------------------");
 			given()
 			.param("programme", "Computer Science")
-			.param("limit",1)
-			.log()
-			.params()
+			.param("limit",2)
 			.when()
 			.get("/list")
 			.then()
+			.log().headers()
 			.statusCode(200);
 		}
+	
+	@DisplayName("Logging the Response Status Line")
+	//@Test
+		public void test003() {
+			System.out.println("-------------Logging Response Status Line-------------------");
+			given()
+			.param("programme", "Computer Science")
+			.param("limit",2)
+			.when()
+			.get("/list")
+			.then()
+			.log().status()
+			.statusCode(200);
+		}
+	
+	
+	@DisplayName("Logging the Response Body")
+	//@Test
+		public void test004() {
+			System.out.println("-------------Logging Response Body-------------------");
+			given()
+			.param("programme", "Computer Science")
+			.param("limit",2)
+			.when()
+			.get("/list")
+			.then()
+			.log().body()
+			.statusCode(200);
+		}
+	
+	@DisplayName("Loggin the response if there is a failure")
+	@Test
+	public void test005()
+	{
+		System.out.println("-------------Logging IN case if there is a failure -------------------");
+		given()
+		.param("programme", "Computer Science")
+		.param("limit",-1)
+		.when()
+		.get("/list")
+		.then()
+		.log().ifError()
+		.statusCode(200);
+	}
 }
